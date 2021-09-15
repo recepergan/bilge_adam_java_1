249d3ee6fab38517e19f809a654e96ab71cf5145
recepergan <rcpergan@gmail.com>
2021-09-15 16:19:43 +0300

    first commit

---
+++ src/com/bilgeadam/examples/Examples_004_DerecedenFahranayt.java
@@ -0,0 +1,9 @@
+package com.bilgeadam.examples;
+
+public class Examples_004_DerecedenFahranayt {
+	public static void main(String[] args) {
+		int f = 80;
+		double c = (f - 32) * (5d / 9d);
+		System.out.println(c);
+	}
+}
---
+++ src/com/bilgeadam/examples/Examples_005_VucutKıtleIndeksi.java
@@ -0,0 +1,10 @@
+package com.bilgeadam.examples;
+
+public class Examples_005_VucutKýtleIndeksi {
+	public static void main(String[] args) {
+		int kýlo = 75;
+		double boy = 1.80;
+		double vki = kýlo / (boy * boy);
+		System.out.println(vki);
+	}
+}
